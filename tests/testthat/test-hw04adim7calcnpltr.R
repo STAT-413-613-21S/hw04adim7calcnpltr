6feb428eff315bcecb8647927117bfdcae545e5d
Adim Okwudishu <adimkris@gmail.com>
2021-03-01 15:29:38 -0500

    updated test file

--- tests/testthat/test-hw04adim7calcnpltr.R
+++ tests/testthat/test-hw04adim7calcnpltr.R
@@ -1,9 +1,9 @@
-test_that("calcn()", {
+test_that("calcn() calculates sequence in vector given n", {
   x <- c(11,1,130)
   n <- 15
   n_int <- (as.integer(n))
   x_test <- c(x, n)
 
-  expect_equal(calcn(x, n_int))
-  expect_equal(calcn(x, n))
+  expect_equal(calcn(x, n_int), expected = 109.64136)
+  expect_equal(calcn(x, n), expected = 109.64136)
 })
